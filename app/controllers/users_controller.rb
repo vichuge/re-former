7f368933ffe68bdc33498fe8b9e81c1c7b7834ab
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-01 16:52:09 -0600

    Creating form with active records

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
-    	if @user.save
-        redirect_to @user
-      else
-        render :new
-      end
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
-    private
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
