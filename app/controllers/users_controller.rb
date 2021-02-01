e86b1a6f1bae0d332da1dbf3e17b3bffb4cb690b
anapdh <ana_d_hubner@hotmail.com>
2021-02-01 18:48:05 -0300

    add validation for controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
+  def create
+    @user = User.new(user_params)
 
-        if @user.save
-            redirect_to @user
-        else
-            render :new
-        end
-    end
+    	if @user.save
+        redirect_to @user
+      else
+        render :new
+      end
+  end
+
+    private
+      def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
 end
