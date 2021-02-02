fc6efef2078896ba83bb7235003635506c0b106f
anapdh <ana_d_hubner@hotmail.com>
2021-02-02 17:33:48 -0300

    Creating edit/update def

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
       end
    end
 
+   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
    private
      def user_params
       params.require(:user).permit(:username, :email, :password)
