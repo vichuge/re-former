d9336dcb2573f6691c3e42d355eb2781a1b876a7
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-02 11:35:51 -0600

    Creating form_tag form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
 
-   def create
-    # @user = User.new(params[:user])
+  def create
+    #@user = User.new(params[:user])
     @user = User.new(user_params)
-       if @user.save
+      if @user.save
         redirect_to new_user_path
       else
         render :new
