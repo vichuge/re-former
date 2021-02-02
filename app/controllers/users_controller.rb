83a301026f0efb61b3fbefe628db29469112353b
anapdh <ana_d_hubner@hotmail.com>
2021-02-02 13:26:22 -0300

    Fixing the form values to work on server, changing controller for create, add user params, making form work properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
-  def create
+   def create
+    # @user = User.new(params[:user])
     @user = User.new(user_params)
+       if @user.save
+        redirect_to new_user_path
+      else
+        render :new
+      end
+   end
 
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
-    end
-  end
-
-  private
-    def user_params
+   private
+     def user_params
       params.require(:user).permit(:username, :email, :password)
-    end
+     end
 end
