3d08c9051345f3c70e60efb5d6f670dd633b4847
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-02 14:56:52 -0600

    Fix linters errors with rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(params[:user])
+    # @user = User.new(params[:user])
     @user = User.new(user_params)
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new
-      end
-   end
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
-   def edit
+  def edit
     @user = User.find(params[:id])
   end
 
@@ -27,8 +27,9 @@ class UsersController < ApplicationController
     end
   end
 
-   private
-     def user_params
-      params.require(:user).permit(:username, :email, :password)
-     end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,14 +12,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_202600) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema.define(version: 20_210_201_202_600) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.string 'password'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
